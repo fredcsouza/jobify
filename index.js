6b649fe849019393d0bb2c452d889a81a345a384
fredcsouza <fredcsouza@gmail.com>
2019-05-02 14:05:10 -0300

    bloqueando aceeso ao admin pra quem nao for localhost

--- index.js
+++ index.js
@@ -9,9 +9,19 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (req, res, next) => {
+    // if(req.hostname === 'localhost'){
+    //     next()
+    // }else{
+    //     res.send('Not allowed')
+    // }
+    (req.hostname === 'localhost') ? next() : res.send('Not allowed')
+    
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', async (request, response) => {
